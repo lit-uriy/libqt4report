b986f3dbace0b78a7a7f72781d743531b6c3ce05
arescleb <arescleb@58161048-842e-499e-a2c8-44584f2a5f65>
2012-09-19 13:57:36 +0000

    First import : * Validate an xml file

---
+++ src/example/example.cpp
@@ -0,0 +1,31 @@
+//------------------------------------------------------------------------------
+#include <libqt4report.h>
+#include <iostream>
+//------------------------------------------------------------------------------
+using std::cout;
+using std::endl;
+//------------------------------------------------------------------------------
+int main(int argc, char **argv) {
+	if(argc < 2) {
+		cout << "Usage: example file [[file] ... ]" << endl;
+		return 1;
+	}
+
+	for(int i=1;i<argc;i++) {	
+		QFile *xmlFile=new QFile(argv[i]);
+		if(xmlFile->open(QIODevice::ReadOnly)) {
+			if(libqt4report::validDocument(xmlFile)) {
+				cout << argv[i] << " is valid !" << endl;
+			}else {
+				cout << argv[i] << " is not valid !" << endl;
+			}
+		}else {
+			cout << argv[i] << " not found !" << endl;
+		}
+		
+		delete xmlFile;
+	}
+	
+	return 0;
+}
+//------------------------------------------------------------------------------
---
+++ src/libqt4report.cpp
@@ -0,0 +1,31 @@
+//------------------------------------------------------------------------------
+#include <QXmlSchema>
+#include <QXmlSchemaValidator>
+#include <QtDebug>
+#include "libqt4report.h"
+//------------------------------------------------------------------------------
+namespace libqt4report {
+	bool validDocument(QFile *docFile) {
+		QXmlSchema *xmlSchema=new QXmlSchema();
+		QFile *xsdFile=new QFile(":/libqt4report/schema/libqt4report.xsd");
+		xsdFile->open(QIODevice::ReadOnly);
+		bool ret=false;
+		
+		xmlSchema->load(xsdFile, QUrl::fromLocalFile(xsdFile->fileName()));
+		if(xmlSchema->isValid()) {
+			QXmlSchemaValidator *validator=new QXmlSchemaValidator(*xmlSchema);
+			
+			if(validator->validate(docFile, QUrl::fromLocalFile(docFile->fileName()))) {
+				ret=true;
+			}
+			
+			delete validator;
+		}
+		
+		delete xsdFile;
+		delete xmlSchema;	
+		
+		return ret;
+	}
+} //namespace
+//------------------------------------------------------------------------------
