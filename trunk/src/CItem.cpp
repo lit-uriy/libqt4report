b4d261e7d6c79836d45eb30c7ccc328fd201abfa
arescleb <arescleb@58161048-842e-499e-a2c8-44584f2a5f65>
2012-10-18 17:33:27 +0000

    Filed format

--- trunk/src/CItem.cpp
+++ trunk/src/CItem.cpp
@@ -9,7 +9,7 @@
 namespace libqt4report {
 	QString CItemText::toSvg(int &y) {
 		double coef=CPrintableObject::getCoef();
-		QString value=CPrintableObject::xmlEncode(getFormatedValue(attributes.value("format")));
+		QString value=CPrintableObject::xmlEncode(getValue().toString());
 		QString align="";
 		QString style="style='";
 		CFont *font=CFonts::getInstance()->getFont(attributes.value("fontId"));
@@ -32,75 +32,6 @@ namespace libqt4report {
 			.arg((int)(attributes.value("x").toDouble()*coef))
 			.arg((int)((attributes.value("y").toDouble())*coef)+y)
 			.arg(value);
-	}
-	//------------------------------------------------------------------------------
-	QString CItemText::getFormatedValue(QString format) {
-		QVariant value=getValue();
-		
-		if(format != "") {
-			QString ret;
-			
-			switch(format.at(0).toAscii()) {
-				case 'd':
-					ret=value.toDate().toString("yyyy-MM-dd");
-					break;
-				case 'i':
-					return getFormatedIntValue(format);
-					break;
-				case 'r':
-					return getFormatedDoubleValue(format);
-					break;
-				case 's': 
-					return getFormatedStringValue(format);
-				default:
-					break;
-			}
-			
-			return ret;
-		}else {
-			return value.toString();
-		}
-	}
-	//------------------------------------------------------------------------------
-	QString CItemText::getFormatedStringValue(QString format) {
-		QRegExp regExp("s(([0-9]*)(([lr])(\\S+)))?");
-		QString value=getValue().toString();
-		
-		if(regExp.indexIn(format, 0) != -1) {
-			int len;
-			QChar pad;
-			QString padDir;
-			
-			len=regExp.cap(2).toInt();
-			padDir=regExp.cap(4);
-			pad=regExp.cap(5).at(0);
-			
-			if(padDir == "l") {
-				return value.rightJustified(len, pad, false);
-			}else {
-				return value.leftJustified(len, pad, false);
-			}
-		}
-		
-		return value;
-	}
-	//------------------------------------------------------------------------------
-	QString CItemText::getFormatedIntValue(QString format) {
-		return QString("%L1").arg(getValue().toInt());
-	}
-	//------------------------------------------------------------------------------
-	QString CItemText::getFormatedDoubleValue(QString format) {
-		QRegExp regExp("r(\\.([0-9]*))?");
-		QString value;
-		
-		if(regExp.indexIn(format, 0) != -1) {
-			int nbDecimaux;
-			
-			nbDecimaux=regExp.cap(2).toInt();
-			value=QString("%L1").arg(getValue().toDouble(), 0, 'f', nbDecimaux != 0 ? nbDecimaux : -1);
-		}
-		
-		return value;
 	}
 	//------------------------------------------------------------------------------
 	QVariant CItemTextFieldObject::getValue(void) {
@@ -156,5 +87,6 @@ namespace libqt4report {
 		return QString("<rect x='%1' y='%2' width='%3' height='%4' style='stroke:"+color+";stroke-width:1;fill:none;' />")
 		.arg(x).arg(yR).arg(width).arg(height);
 	}
+	//------------------------------------------------------------------------------
 }//namespace
 //------------------------------------------------------------------------------
