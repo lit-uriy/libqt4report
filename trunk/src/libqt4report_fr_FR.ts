0754907325c56b25c2fc5bfe2c91ef2d81edc406
arescleb <arescleb@58161048-842e-499e-a2c8-44584f2a5f65>
2012-11-02 18:00:29 +0000

    Translate

--- trunk/src/libqt4report_fr_FR.ts
+++ trunk/src/libqt4report_fr_FR.ts
@@ -5,23 +5,23 @@
 <context>
     <name>QObject</name>
     <message>
-        <location filename="CDocument.cpp" line="43"/>
-        <location filename="CDocument.cpp" line="51"/>
+        <location filename="CDocument.cpp" line="55"/>
+        <location filename="CDocument.cpp" line="72"/>
         <source>Database error</source>
         <translation type="unfinished">Erreur de base de données</translation>
     </message>
     <message>
-        <location filename="CDocument.cpp" line="60"/>
+        <location filename="CDocument.cpp" line="81"/>
         <source>Processing error</source>
         <translation type="unfinished">Erreur de traitement</translation>
     </message>
     <message>
-        <location filename="CReport.cpp" line="75"/>
+        <location filename="libqt4report.cpp" line="93"/>
         <source>Invalid file</source>
         <translation type="unfinished">Fichier invalide</translation>
     </message>
     <message>
-        <location filename="CReport.cpp" line="76"/>
+        <location filename="libqt4report.cpp" line="94"/>
         <source>Unable to parse the file : </source>
         <translation type="unfinished">Impossible de </translation>
     </message>
--- trunk/src/viewer/viewer_fr_FR.ts
+++ trunk/src/viewer/viewer_fr_FR.ts
@@ -5,7 +5,7 @@
 <context>
     <name>CMainFrm</name>
     <message>
-        <location filename="CMainFrm.cpp" line="+45"/>
+        <location filename="CMainFrm.cpp" line="+54"/>
         <source>Validation error</source>
         <translation type="unfinished">Erreur de validation</translation>
     </message>
@@ -15,7 +15,7 @@
         <translation type="unfinished">Impossible de valider le document, êtes vous sûre qu&apos;il s&apos;agisse bien d&apos;un document xlqr</translation>
     </message>
     <message>
-        <location line="+8"/>
+        <location line="+10"/>
         <source>Error</source>
         <translation type="unfinished">Erreur</translation>
     </message>
@@ -25,7 +25,7 @@
         <translation type="unfinished">Impossible d&apos;ouvir le document</translation>
     </message>
     <message>
-        <location line="+15"/>
+        <location line="+26"/>
         <source>Open</source>
         <translation type="unfinished">Ouvir</translation>
     </message>
@@ -34,63 +34,49 @@
         <source>xlqr file (*.xlqr)</source>
         <translation type="unfinished">Fichier xlqr (*.xlqr)</translation>
     </message>
+    <message>
+        <location line="+131"/>
+        <source>Value for</source>
+        <translation type="unfinished">Valeur de</translation>
+    </message>
 </context>
 <context>
     <name>MainFrm</name>
     <message>
-        <location filename="CMainFrm.ui"/>
-        <source>MainWindow</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location/>
         <source>&amp;File</source>
-        <translation type="unfinished">&amp;Fichier</translation>
+        <translation type="obsolete">&amp;Fichier</translation>
     </message>
     <message>
-        <location/>
-        <source>toolBar</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location/>
         <source>First page</source>
-        <translation type="unfinished">Première page</translation>
+        <translation type="obsolete">Première page</translation>
     </message>
     <message>
-        <location/>
         <source>Previous page</source>
-        <translation type="unfinished">Page précédente</translation>
+        <translation type="obsolete">Page précédente</translation>
     </message>
     <message>
-        <location/>
         <source>Next page</source>
-        <translation type="unfinished">Page suivante</translation>
+        <translation type="obsolete">Page suivante</translation>
     </message>
     <message>
-        <location/>
         <source>Last page</source>
-        <translation type="unfinished">Dernière page</translation>
+        <translation type="obsolete">Dernière page</translation>
     </message>
     <message>
-        <location/>
         <source>&amp;Open</source>
-        <translation type="unfinished">&amp;Ouvir</translation>
+        <translation type="obsolete">&amp;Ouvir</translation>
     </message>
     <message>
-        <location/>
         <source>Reload</source>
-        <translation type="unfinished">Recharger</translation>
+        <translation type="obsolete">Recharger</translation>
     </message>
     <message>
-        <location/>
         <source>&amp;Close</source>
-        <translation type="unfinished">&amp;Fermer</translation>
+        <translation type="obsolete">&amp;Fermer</translation>
     </message>
     <message>
-        <location/>
         <source>&amp;Quit</source>
-        <translation type="unfinished">&amp;Quitter</translation>
+        <translation type="obsolete">&amp;Quitter</translation>
     </message>
 </context>
 </TS>
