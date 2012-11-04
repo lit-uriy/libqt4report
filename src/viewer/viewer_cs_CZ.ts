1058cb9502e8c65dedf757c5b37efdf99dec7ac8
arescleb <arescleb@58161048-842e-499e-a2c8-44584f2a5f65>
2012-11-04 16:44:42 +0000

    Czech localization

---
+++ src/viewer/viewer_cs_CZ.ts
@@ -0,0 +1,96 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="cs_CZ">
+<defaultcodec>UTF-8</defaultcodec>
+<context>
+    <name>CMainFrm</name>
+    <message>
+        <location filename="CMainFrm.cpp" line="+45"/>
+        <source>Validation error</source>
+        <translation>Chyba při schvalování</translation>
+    </message>
+    <message>
+        <location line="+0"/>
+        <source>Impossible to validate document, are your sure that is it a lxqr file ?</source>
+        <translation>Dokument nelze schválit. Jste si jistý, že se jedná o dokument xlqr?</translation>
+    </message>
+    <message>
+        <location line="+8"/>
+        <source>Error</source>
+        <translation>Chyba</translation>
+    </message>
+    <message>
+        <location line="+0"/>
+        <source>Impossible to open document</source>
+        <translation>Dokument nelze otevřít</translation>
+    </message>
+    <message>
+        <location line="+15"/>
+        <source>Open</source>
+        <translation>Otevřít</translation>
+    </message>
+    <message>
+        <location line="+0"/>
+        <source>xlqr file (*.xlqr)</source>
+        <translation>Soubor xlqr (*.xlqr)</translation>
+    </message>
+</context>
+<context>
+    <name>MainFrm</name>
+    <message>
+        <location filename="CMainFrm.ui"/>
+        <source>MainWindow</source>
+        <translation>Hlavní okno</translation>
+    </message>
+    <message>
+        <location/>
+        <source>&amp;File</source>
+        <translation>&amp;Soubor</translation>
+    </message>
+    <message>
+        <location/>
+        <source>toolBar</source>
+        <translation>Nástrojový pruh</translation>
+    </message>
+    <message>
+        <location/>
+        <source>First page</source>
+        <translation>První strana</translation>
+    </message>
+    <message>
+        <location/>
+        <source>Previous page</source>
+        <translation>Předchozí strana</translation>
+    </message>
+    <message>
+        <location/>
+        <source>Next page</source>
+        <translation>Další strana</translation>
+    </message>
+    <message>
+        <location/>
+        <source>Last page</source>
+        <translation>Poslední strana</translation>
+    </message>
+    <message>
+        <location/>
+        <source>&amp;Open</source>
+        <translation>&amp;Otevřít</translation>
+    </message>
+    <message>
+        <location/>
+        <source>Reload</source>
+        <translation>Nahrát znovu</translation>
+    </message>
+    <message>
+        <location/>
+        <source>&amp;Close</source>
+        <translation>&amp;Zavřít</translation>
+    </message>
+    <message>
+        <location/>
+        <source>&amp;Quit</source>
+        <translation>&amp;Ukončit</translation>
+    </message>
+</context>
+</TS>
