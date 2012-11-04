921c7aaf94982b4c34470717d0c06e7106a5ad7e
arescleb <arescleb@58161048-842e-499e-a2c8-44584f2a5f65>
2012-11-04 16:44:42 +0000

    Czech localization

---
+++ trunk/src/libqt4report_cs_CZ.ts
@@ -0,0 +1,29 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="cs_CZ">
+<defaultcodec>UTF-8</defaultcodec>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="CDocument.cpp" line="43"/>
+        <location filename="CDocument.cpp" line="51"/>
+        <source>Database error</source>
+        <translation>Chyba databáze</translation>
+    </message>
+    <message>
+        <location filename="CDocument.cpp" line="60"/>
+        <source>Processing error</source>
+        <translation>Chyba při zpracování</translation>
+    </message>
+    <message>
+        <location filename="CReport.cpp" line="75"/>
+        <source>Invalid file</source>
+        <translation>Neplatný soubor</translation>
+    </message>
+    <message>
+        <location filename="CReport.cpp" line="76"/>
+        <source>Unable to parse the file : </source>
+        <translation>Nelze zpracovat soubor: </translation>
+    </message>
+</context>
+</TS>
